b7914c44bb6fbea7727fd008147c707ed8a88409
Be-student <presentsong@naver.com>
2022-11-10 01:04:14 +0900

    feat(LottoScoreDto): View에 데이터 전송 객체 추가

---
+++ src/main/java/lotto/domain/LottoScoreDto.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+public class LottoScoreDto {
+    private final int three;
+    private final int four;
+    private final int five;
+    private final int fiveBonus;
+    private final int six;
+
+    private final double rate;
+
+    public LottoScoreDto(int three, int four, int five, int fiveBonus, int six, double rate) {
+        this.three = three;
+        this.four = four;
+        this.five = five;
+        this.fiveBonus = fiveBonus;
+        this.six = six;
+        this.rate = rate;
+    }
+
+    public int getThree() {
+        return three;
+    }
+
+    public int getFour() {
+        return four;
+    }
+
+    public int getFive() {
+        return five;
+    }
+
+    public int getFiveBonus() {
+        return fiveBonus;
+    }
+
+    public int getSix() {
+        return six;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+}

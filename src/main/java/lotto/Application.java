45d0ed3514368207ead8bc206cd67e54a6c1c8be
Be-student <presentsong@naver.com>
2022-11-10 16:36:09 +0900

    feat(Application): 루트 코드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import handler.InputHandler;
+import handler.OutputHandler;
+import handler.console.ConsolePrinter;
+import handler.console.ConsoleScanner;
+import lotto.domain.LottoMachine;
+import lotto.domain.LottoRandom;
+import lotto.domain.MissionRandom;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler inputHandler = ConsoleScanner.INSTANCE;
+        OutputHandler outputHandler = ConsolePrinter.INSTANCE;
+        InputView inputView = new InputView(inputHandler);
+        OutputView outputView = new OutputView(outputHandler);
+        LottoRandom lottoRandom = new MissionRandom();
+        LottoMachine lottoMachine = new LottoMachine(inputView, outputView, lottoRandom);
+
+        try {
+            lottoMachine.run();
+        } catch (Exception e) {
+            System.out.println("[ERROR]" + e.getMessage());
+        }
     }
 }

77ac5ec1b22212cdd1b89011e5463dd45d4d6d00
Be-student <presentsong@naver.com>
2022-11-11 08:18:51 +0900

    feat(MockInputHandler): 입력값을 모킹하는 기능 추가
    setExpected를 통해 설정한 값이 순서대로 읽어짐

---
+++ src/test/java/handler/MockInputHandler.java
@@ -0,0 +1,16 @@
+package handler;
+
+public class MockInputHandler implements InputHandler {
+
+    private String[] expected;
+    private int current = 0;
+
+    public void setExpected(String... expected) {
+        this.expected = expected;
+    }
+
+    @Override
+    public String readLine() {
+        return expected[current++];
+    }
+}

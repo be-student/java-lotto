ad302afacdd6200e89a474f874d4318180bb3b96
Be-student <presentsong@naver.com>
2022-11-11 09:56:07 +0900

    refactor: 메시지 명칭 변경

--- src/main/java/lotto/config/ErrorMessages.java
+++ src/main/java/lotto/config/ErrorMessages.java
@@ -4,9 +4,9 @@ import static lotto.config.LottoConstants.LOTTO_END_INCLUSIVE;
 import static lotto.config.LottoConstants.LOTTO_START_INCLUSIVE;
 
 public enum ErrorMessages {
-    AMOUNT_NOT_NUMBER("구입 금액이 숫자만으로 이루어지지 않았습니다"),
-    AMOUNT_NOT_THOUSANDS("구입 금액이 1000으로 나누어 떨어지지 않음"),
-    AMOUNT_IS_NOT_NATURE("구입 금액이 양수가 아님"),
+    AMOUNT_NOT_NUMBER_ERROR("구입 금액이 숫자만으로 이루어지지 않았습니다"),
+    AMOUNT_NOT_THOUSANDS_ERROR("구입 금액이 1000으로 나누어 떨어지지 않음"),
+    AMOUNT_NOT_POSITIVE_ERROR("구입 금액이 양수가 아님"),
 
     LOTTERY_FORMAT_ERROR("당첨 번호가 숫자,숫자 형식이 아닙니다"),
     LOTTERY_COUNT_ERROR("당첨 번호가 6개가 아닙니다"),

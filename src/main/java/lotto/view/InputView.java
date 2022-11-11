c529bebd8acadfea19128e0b2fc8798266ab5200
Be-student <presentsong@naver.com>
2022-11-11 17:46:54 +0900

    feat(InputView): 입력을 받는 view 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,26 @@
+package lotto.view;
+
+import handler.InputHandler;
+import lotto.domain.Lotto;
+
+public class InputView {
+    private final InputHandler inputHandler;
+
+    private InputView(InputHandler inputHandler) {
+        this.inputHandler = inputHandler;
+    }
+
+    public static InputView createWithHandler(InputHandler inputHandler) {
+        return new InputView(inputHandler);
+    }
+
+    public Lotto getPlayerLottery() {
+        String input = inputHandler.readLine();
+        return InputValidator.validateLotteryNumbers(input);
+    }
+
+    public int getPlayerAmount() {
+        String input = inputHandler.readLine();
+        return InputValidator.validateAmount(input);
+    }
+}

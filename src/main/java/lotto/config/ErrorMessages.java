0d12fb434ea5b13819c01becfd0596a31c1afdcb
Be-student <presentsong@naver.com>
2022-11-11 09:45:14 +0900

    feat(ErrorMessage): 에러 메시지 기능 추가
    에러 메시지를 enum으로 관리함

---
+++ src/main/java/lotto/config/ErrorMessages.java
@@ -0,0 +1,29 @@
+package lotto.config;
+
+import static lotto.config.LottoConstants.LOTTO_END_INCLUSIVE;
+import static lotto.config.LottoConstants.LOTTO_START_INCLUSIVE;
+
+public enum ErrorMessages {
+    AMOUNT_NOT_NUMBER("구입 금액이 숫자만으로 이루어지지 않았습니다"),
+    AMOUNT_NOT_THOUSANDS("구입 금액이 1000으로 나누어 떨어지지 않음"),
+    AMOUNT_IS_NOT_NATURE("구입 금액이 양수가 아님"),
+
+    LOTTERY_FORMAT_ERROR("당첨 번호가 숫자,숫자 형식이 아닙니다"),
+    LOTTERY_COUNT_ERROR("당첨 번호가 6개가 아닙니다"),
+    LOTTERY_SIZE_ERROR("당첨 번호가 " + LOTTO_START_INCLUSIVE + "부터 " + LOTTO_END_INCLUSIVE + "가 아닙니다"),
+    LOTTERY_DUPLICATE_ERROR("당첨 번호에 중복이 있습니다"),
+
+    BONUS_FORMAT_ERROR("보너스 번호가 숫자만으로 이루어지지 않음"),
+    BONUS_SIZE_ERROR("당첨 번호가 " + LOTTO_START_INCLUSIVE + "부터 " + LOTTO_END_INCLUSIVE + "가 아닙니다"),
+    BONUS_DUPLICATE_ERROR("당첨 번호와 보너스 번호의 중복이 있습니다");
+
+    private final String errorMessage;
+
+    ErrorMessages(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
---
+++ src/main/java/lotto/config/LottoConstants.java
@@ -0,0 +1,10 @@
+package lotto.config;
+
+public class LottoConstants {
+    public static final String ERROR_PREFIX = "[ERROR]";
+    public static final int LOTTO_START_INCLUSIVE = 1;
+    public static final int LOTTO_END_INCLUSIVE = 45;
+
+    private LottoConstants() {
+    }
+}

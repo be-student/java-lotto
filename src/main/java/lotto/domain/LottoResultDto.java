f72b382fe6d76fdf7de020e34a73ab41686b18de
Be-student <presentsong@naver.com>
2022-12-04 09:54:23 +0900

    feat: 로또들을 옮기는 dto 추가

--- src/main/java/lotto/domain/LottoResultDto.java
+++ src/main/java/lotto/domain/LottoResultDto.java
@@ -1,8 +1,16 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LottoResultDto {
 
-    public LottoResultDto() {
+    private final List<List<Integer>> lottos;
+
+    public LottoResultDto(List<List<Integer>> lottos) {
+        this.lottos = lottos;
     }
 
+    public List<List<Integer>> getLottos() {
+        return lottos;
+    }
 }

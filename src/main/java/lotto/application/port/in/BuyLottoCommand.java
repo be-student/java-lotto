ca72b1153a5ec5496c62be42abc51845eecb68ae
Be-student <presentsong@naver.com>
2022-12-04 16:04:29 +0900

    refactor: 숫자로 변환하는 거 다 validator로 모음

--- src/main/java/lotto/application/port/in/BuyLottoCommand.java
+++ src/main/java/lotto/application/port/in/BuyLottoCommand.java
@@ -3,6 +3,8 @@ package lotto.application.port.in;
 
 import static lotto.util.Validator.isNumber;
 
+import lotto.util.Validator;
+
 public class BuyLottoCommand {
 
     private static final String INPUT_NULL_MESSAGE = "입력에 null 이 있으면 안됩니다";
@@ -11,7 +13,7 @@ public class BuyLottoCommand {
 
     public BuyLottoCommand(String input) {
         validate(input);
-        amount = toLong(input);
+        amount = Validator.toLong(input);
     }
 
     private void validate(String input) {
@@ -20,14 +22,6 @@ public class BuyLottoCommand {
         }
         if (!isNumber(input)) {
             throw new IllegalArgumentException(NOT_NUMBER_MESSAGE);
-        }
-    }
-
-    private Long toLong(String input) {
-        try {
-            return Long.parseLong(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자 범위를 초과했습니다");
         }
     }
 
--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -12,4 +12,20 @@ public class Validator {
     public static boolean isNumber(String input) {
         return NUMBER.matcher(input).matches();
     }
+
+    public static int toInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자로 변환할 수 없습니다");
+        }
+    }
+
+    public static long toLong(String input) {
+        try {
+            return Long.parseLong(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자로 변환할 수 없습니다");
+        }
+    }
 }

0b673de5c7c73f660068cc4db825b430179588a8
Be-student <presentsong@naver.com>
2022-12-04 19:53:17 +0900

    feat: 메인 함수에서 실행하는 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import lotto.adapter.in.client.InputView;
+import lotto.adapter.in.client.LottoClient;
+import lotto.adapter.in.client.OutputView;
+import lotto.application.service.LottoApplication;
+import lotto.domain.LottoGenerator;
+import lotto.domain.RandomLottoNumbersGenerator;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new LottoClient(new InputView(), new OutputView(),
+                new LottoApplication(new LottoGenerator(new RandomLottoNumbersGenerator()))).run();
+
     }
 }
